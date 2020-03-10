593369784b5027bcff4f04359d7275237983ebab
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-10 22:02:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def gross_for_director(director_data)
   gross = 0
   i = 0
   while i < director_data[:movies].length do
-    movie = director_data[i][:movies]
+    movie = director_data[:movies][i]
     gross += movie[:worldwide_gross]
     i += 1
   end
