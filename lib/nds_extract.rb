a3db144cdedf363d0d82e53647b88ed0a1bea419
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-10 21:57:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,8 @@ def directors_totals(nds)
   i = 0
   while i < nds.length do
     name = nds[i][:name]
-    d_info = gross_for_director(nds[i]) 
+    d_info = gross_for_director(nds[i])
+    result[name] = d_info[name]
     i += 1
   end
 
