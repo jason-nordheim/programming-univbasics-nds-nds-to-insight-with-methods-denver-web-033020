82d917b934df96fdd661594120ef990d21680173
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-10 21:54:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,13 +5,14 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
   # Array => Hash (:name, :movies => Array[Hash{:title,:studio,:worldwide_grosses,:release_year}])
+  name = director_data[:name]
   gross = 0
   i = 0
   while i < director_data.length do
     movie = director_data[i]
     gross += movie[:worldwide_gross]
   end
-
+  result = {}
 
 end
 
